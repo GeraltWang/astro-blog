ad1eaf3b3143c78eb261fee7fcae0dd93a7323a3
王昶 <wgeralt@outlook.com>
2023-09-13 18:05:54 +0800

    feat: posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
+/*
+ * @Author: 王昶
+ * @Date: 2023-09-13 02:29:39
+ * @LastEditors: 王昶
+ * @LastEditTime: 2023-09-13 17:12:59
+ * @FilePath: /astro-blog/astro.config.mjs
+ * @Description: 
+ * 
+ * 
+ */
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    mdx()
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      langs: [],
+      wrap: true
+    }
+  },
+});
